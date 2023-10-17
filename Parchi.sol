73659710f562a233bf53a78365c4f7e56d32a43c
Juan Xavier Valverde <juanxaviervm@hotmail.com>
2023-10-17 16:57:55 -0500

    wip

--- Parchi.sol
+++ Parchi.sol
@@ -7,14 +7,12 @@ enum GameStatus {
 }
 
 contract ParchiThap {
-  GameStatus internal gameStatus;
-  address private immutable OWNER;
-  uint88 internal startTime;
-  uint8 playerInTurnIndex;
-
-  address[4] public players;
-  uint8[4][4] public gameState;
-
+  address private immutable OWNER = msg.sender;
+  GameStatus private gameStatus;
+  uint88 private startTime;
+  uint8 private playerInTurnIndex;
+  address[4] private players;
+  uint8[4][4] private gameState;
   mapping(address => uint256) private wins;
 
   function _onlyOwner() private view {
@@ -25,17 +23,20 @@ contract ParchiThap {
     if (gameStatus != _gameStatus) revert();
   }
 
-  constructor() {
-    OWNER = msg.sender;
+  function _isPlayer(address player) private view returns (bool b) {
+    unchecked {
+      for (uint256 i; i < 4; ++i) {
+        b = player == players[i];
+        if (b) break;
+      }
+    }
   }
 
-  function _addPlayers(address[4] memory _players) private {
+  function _addPlayers(address[4] memory _players) public {
     unchecked {
-      uint256 i = 4;
-      while (i > 0) {
-        if (players[i] != address(0) && players[i] != msg.sender) players[i] = _players[i];
-        else revert();
-        --i;
+      for (uint256 i; i < 4; ++i) {
+        if (_players[i] == address(0) || _players[i] == msg.sender) revert();
+        players[i] = _players[i];
       }
     }
   }
@@ -51,7 +52,7 @@ contract ParchiThap {
     }
   }
 
-  function _setState(uint8[4][4] memory _state) private pure {
+  function _setState(uint8[4][4] memory _state) private {
     unchecked {
       uint256 total;
       for (uint256 i; i < 4; ++i) {
@@ -60,10 +61,11 @@ contract ParchiThap {
         total = _state[0][i] + _state[1][i] + _state[2][i] + _state[3][i];
         if (total != 4) revert();
       }
+      gameState = _state;
     }
   }
 
-  function _newGameState() public view returns (uint8[4][4] memory _parchis) {
+  function _newGameState() private view returns (uint8[4][4] memory _parchis) {
     uint256 pos;
     uint256 rand = uint256(keccak256(abi.encodePacked(block.timestamp)));
     uint256 i;
@@ -71,17 +73,23 @@ contract ParchiThap {
 
     unchecked {
       while (i < 16) {
-        // i 0, rand 1234
         if (((rand >> pos) & 1) == 1) {
-          uint a = pos % 4; // the whole loop would go {0,0,0,0,1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4}
-          while (z[a] == 4) a = ++a % 4; // if value is 4, change is to a value between 0 and 3
-          ++_parchis[a][i / 4]; // update horizontal values 0
+          uint a = pos % 4;
+          while (z[a] == 4) a = ++a % 4;
+          ++_parchis[a][i / 4];
           ++z[a];
           ++i;
         }
         ++pos;
       }
     }
+  }
+
+  function _resetGame() private {
+    playerInTurnIndex = 0;
+    delete players;
+    delete gameState;
+    gameStatus = GameStatus.Inactive;
   }
 
   /* ****************************************************** */
@@ -119,24 +127,18 @@ contract ParchiThap {
 
       uint8 nextPlayerInTurnIndex = (++_playerInTurnIndex) % 4;
 
-      if (playerParchis[_type] > 0) {
-        --gameState[_playerInTurnIndex][_type];
-        ++gameState[nextPlayerInTurnIndex][_type];
+      if (playerParchis[_type - 1] > 0) {
+        --gameState[_playerInTurnIndex][_type - 1];
+        ++gameState[nextPlayerInTurnIndex][_type - 1];
       } else revert();
 
       playerInTurnIndex = nextPlayerInTurnIndex;
     }
   }
 
-  function endGame() public {
-    if (!isPlayer(msg.sender)) revert();
-    if (block.timestamp < startTime + 1 hours) revert();
-    _resetGame();
-  }
-
   function claimWin() external {
     unchecked {
-      if (!isPlayer(msg.sender)) revert();
+      if (!_isPlayer(msg.sender)) revert();
       _onlyInStatus(GameStatus.Active);
       uint256 playerIndex = _getIndex();
       uint256 i = 4;
@@ -151,28 +153,10 @@ contract ParchiThap {
     }
   }
 
-  function isPlayer(address player) internal view returns (bool b) {
-    unchecked {
-      uint256 i = 4;
-
-      while (i > 0) {
-        b = player == players[i];
-        --i;
-      }
-    }
-  }
-
-  function _resetGame() internal {
-    playerInTurnIndex = 0;
-    delete players;
-    delete gameState;
-    gameStatus = GameStatus.Inactive;
-  }
-
-  function getState() external view returns (address[4] memory, address turn, uint8[4][4] memory) {
-    _onlyOwner();
-    _onlyInStatus(GameStatus.Active);
-    return (players, players[playerInTurnIndex], gameState);
+  function endGame() external {
+    if (!_isPlayer(msg.sender)) revert();
+    if (block.timestamp < startTime + 1 hours) revert();
+    _resetGame();
   }
 
   function getWins(address player) external view returns (uint256) {
@@ -181,13 +165,22 @@ contract ParchiThap {
 
   function myParchis() external view returns (uint8[4] memory) {
     unchecked {
-      if (!isPlayer(msg.sender)) revert();
+      if (!_isPlayer(msg.sender)) revert();
       uint256 index = _getIndex();
       return gameState[index];
     }
   }
+
+  function getState() external view returns (address[4] memory, address turn, uint8[4][4] memory) {
+    _onlyOwner();
+    _onlyInStatus(GameStatus.Active);
+    return (players, players[playerInTurnIndex], gameState);
+  }
 }
+
 // 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2
 // 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db
 // 0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB
 // 0x617F2E2fD72FD9D5503197092aC168c91465E7f2
+// ["0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2","0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db","0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB","0x617F2E2fD72FD9D5503197092aC168c91465E7f2"]
+//
